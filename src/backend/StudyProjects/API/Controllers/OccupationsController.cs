9154adb4a7e52c5b394efaae076d632d49e4cdf6
Dara Keon <lc.sales.duarte@gmail.com>
2024-02-19 14:21:44 +0000

    add backend data example

---
+++ src/backend/StudyProjects/API/Controllers/OccupationsController.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+	[ApiController]
+	public class OccupationsController : ControllerBase
+	{
+		[HttpGet(Name = "GetOccupations")]
+		public IEnumerable<OccupationModel> Get()
+		{
+			var occupations = new List<OccupationModel>();
+			
+			occupations.Add(
+				new OccupationModel(1, "Code", DateTime.Now, DateTime.Now)
+			);
+
+			return occupations;
+		}
+	}
+}
---
+++ src/backend/StudyProjects/API/Models/OccupationModel.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Models;
+
+public class OccupationModel
+{
+	public OccupationModel(Int32 id, String task, DateTime start, DateTime end)
+	{
+		ID = id;
+		Task = task;
+		Start = start;
+		End = end;
+	}
+
+	public Int32 ID { get; set; }
+	public String Task { get; set; }
+	public DateTime Start { get; set; }
+	public DateTime End { get; set; }
+}
