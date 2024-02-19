ea441440cf62343a40d71c9990ef1b8fda1e1f9e
Dara Keon <lc.sales.duarte@gmail.com>
2024-02-19 19:20:05 +0000

    add post occupation model

--- src/backend/StudyProjects/API/Models/OccupationModel.cs
+++ src/backend/StudyProjects/API/Models/OccupationModel.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Models;
+using Npgsql;
+
+namespace API.Models;
 
 public class OccupationModel
 {
@@ -14,4 +16,29 @@ public class OccupationModel
 	public String Task { get; set; }
 	public DateTime Start { get; set; }
 	public DateTime End { get; set; }
+
+	public void Save()
+	{
+		// Configuração para conectar no banco de dados
+		var connectionString = 
+			"Host=localhost;Database=study-projects;Username=user;Password=password";
+
+		// Conexão com o banco sendo criada
+		var dataSource = NpgsqlDataSource.Create(connectionString);
+
+		// Query (código SQL) para inserir novos Occupations no banco
+		var insertQuery = "INSERT INTO Occupations (\"Task\", \"Start\", \"End\")" +
+		                  "    VALUES (@task, @start, @end)";
+
+		// Criação do comando para executar o SQL de insert
+		var command = dataSource.CreateCommand(insertQuery);
+		
+		// Associando dados da query acima com os dados do Model Occupation
+		command.Parameters.AddWithValue("@task", Task);
+		command.Parameters.AddWithValue("@start", Start);
+		command.Parameters.AddWithValue("@end", End);
+
+		// Executando o insert no banco de dados
+		command.ExecuteNonQuery();
+	}
 }
